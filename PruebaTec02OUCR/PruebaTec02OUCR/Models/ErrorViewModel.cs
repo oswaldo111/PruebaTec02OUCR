4d3c57d507ff3fe883941d66228f44e2dd98da67
patotatoman <patoman2005@gmail.com>
2024-02-21 10:01:44 -0600

    Initial Commit

---
+++ PruebaTec02OUCR/PruebaTec02OUCR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaTec02OUCR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
